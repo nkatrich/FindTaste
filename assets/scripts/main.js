2965c25e59e8866d4853454318a3c9c435fae06e
Nikita <nikitakatrich.kzn@gmail.com>
2026-03-03 22:21:02 +0300

    made in HTML header with input search, button with wishlist

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,42 @@
-window.location.hash = "#home"
 const app = document.querySelector(".app")
 
 function renderHome() {
-  app.innerHTML += `
-    <h1>Home</h1>
-    <button data-id="550">Open Movie 550</button>
-  `
+  app.innerHTML = `
+    <section class="home">
+        <section class="recs-slider">
+            <div class="container-recs">
+                <article class="random-recom">
+                    <figure class="card-random-recom">
+                        <img src="" alt="preview of movie">
+                        <figcaption>
+                            <h2 class="title-of-recom"></h2>
+                            <h3 class="date-of-recom"></h3>
+                            <span class="rate-of-recom"><span class="visual-rate-of-recom"></span><span class="percentage-of-rate-of-recom"></span></span>
+                        </figcaption>
+                    </figure>
+                </article>
+            </div>
+        </section>
+        <section class="list-movies">
+            <div class="container-list-movies">
+                <div class="div-title-recomindations-list"><h2 class="title-recomindations-list">Recommendations</h2></div>
+                    <div class="cards-of-recs-list">
+                        <article class="random-recom-card">
+                            <figure class="card-random-recom">
+                                <img src="" alt="preview of movie">
+                                <figcaption>
+                                    <h2 class="title-of-recom"></h2>
+                                    <h3 class="date-of-recom"></h3>
+                                    <span class="rate-of-recom"><span class="visual-rate-of-recom"></span><span class="percentage-of-rate-of-recom"></span></span>
+                                </figcaption>
+                            </figure>
+                        </article>
+                    </div>
+                <div class="observer"></div>
+            </div>
+        </section>
+    </section>
+  `;
 
   const btn = document.querySelector("button")
   btn.addEventListener("click", () => {
@@ -23,7 +54,7 @@ function renderMovie(id) {
 function router() {
   const hash = window.location.hash
 
-  if (hash === "#home") {
+  if (!hash || hash === "#home") {
     renderHome()
     return
   }
