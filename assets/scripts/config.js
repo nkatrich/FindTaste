26bbe49a65581659d499ea101c19e14e163d8b98
Nikita <nikitakatrich.kzn@gmail.com>
2026-03-17 16:03:27 +0300

    tries to fix api key

---
+++ assets/scripts/config.js
@@ -0,0 +1,11 @@
+export const baseURL = "https://api.themoviedb.org/3";
+export const imgURL = 'https://image.tmdb.org/t/p/w1280';
+export const smallImgURL = 'https://image.tmdb.org/t/p/w500';
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer'
+  }
+};
