8c59c00606c3bb512fb0390a15c491ddde446998
Nikita <nikitakatrich.kzn@gmail.com>
2026-03-17 15:31:06 +0300

    have been regenerated API key(2)

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -6,6 +6,6 @@ export const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGQyNmZhYTExNTQ5NTk0NmRiZGY1ZmJmYWI2ZDY0NSIsIm5iZiI6MTc3MTQzMzM5MC4yMDIsInN1YiI6IjY5OTVlZGFlNGZiZWRkMDc0NjFiMDZmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.33XUiTeIQabCIXERPD_uYs8EgEDgG93C_ttnwRaR-OQ'
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OWE3ZDJjZTQ1MDY5ZDU2NGE0OGVkNjQ2YTg5NmMwMiIsIm5iZiI6MTc3MTQzMzM5MC4yMDIsInN1YiI6IjY5OTVlZGFlNGZiZWRkMDc0NjFiMDZmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wFYEJVWk9BjF2axUd7wrERGxc7Kx1vp8rIJMhu9Vu_o'
   }
 };
