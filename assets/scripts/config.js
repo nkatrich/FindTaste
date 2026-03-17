9350f580ef1fd07e70ff8b7a14fbdb55338c9f78
Nikita <nikitakatrich.kzn@gmail.com>
2026-03-17 15:22:40 +0300

    have been regenerated API key for better secure, added config.js in .gitignore

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -6,6 +6,6 @@ export const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjY1MWNiZDgyYmJhNTM0YjNkN2U5N2Y2M2MzZWM1NyIsIm5iZiI6MTc3MTQzMzM5MC4yMDIsInN1YiI6IjY5OTVlZGFlNGZiZWRkMDc0NjFiMDZmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sIddTmKy6pguB1ccLYrkX3Z_KKja-_7NrORj7QZritA'
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NGQyNmZhYTExNTQ5NTk0NmRiZGY1ZmJmYWI2ZDY0NSIsIm5iZiI6MTc3MTQzMzM5MC4yMDIsInN1YiI6IjY5OTVlZGFlNGZiZWRkMDc0NjFiMDZmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.33XUiTeIQabCIXERPD_uYs8EgEDgG93C_ttnwRaR-OQ'
   }
 };
